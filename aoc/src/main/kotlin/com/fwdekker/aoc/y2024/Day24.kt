d258a0594f1c243b9da8d373cb15653ea4129cbf
Florine W. Dekker <florine@fwdekker.com>
2025-12-15 21:21:25 +0100

    Solve AOC2024 day 24

--- aoc/src/main/kotlin/com/fwdekker/aoc/y2024/Day24.kt
+++ aoc/src/main/kotlin/com/fwdekker/aoc/y2024/Day24.kt
@@ -1,198 +1,61 @@
 package com.fwdekker.aoc.y2024
 
 import com.fwdekker.aoc.Day
-import com.fwdekker.std.collections.allDistinct
-import com.fwdekker.std.collections.associateWithIndex
-import com.fwdekker.std.maths.combinations
 import com.fwdekker.std.maths.toLong
 import com.fwdekker.std.sections
-import java.util.SortedSet
 
 
 // See https://adventofcode.com/2024/day/24
-class Day24(
-    sample: Int? = null,
-    private val swapCount: Int = 4,
-    private val operator: (String, String) -> String =
-        { a, b -> (a.bitStringToLong() + b.bitStringToLong()).toBitString() },
-) : Day(year = 2024, day = 24, sample = sample) {
+class Day24(sample: Int? = null) : Day(year = 2024, day = 24, sample = sample) {
     private val sections = input.sections()
     private val circuit = Circuit(
-        startSignals = sections[0].map { it.split(": ") }.associate { it[0] to (it[1] == "1") },
-        gates = sections[1].map { it.split(' ') }.map { Gate.from(it[0], it[2], it[4], it[1]) },
+        signals = sections[0].map { it.split(": ") }.associateTo(mutableMapOf()) { it[0] to (it[1] == "1") },
+        gates = sections[1].map { it.split(' ') }.associate { it[4] to Triple(it[0], it[2], it[1]) },
     )
 
 
-    override fun part1(): Long = circuit.evaluate().bitStringToLong()
+    override fun part1() = circuit.evaluate().fold(0L) { acc, it -> acc * 2L + it.toLong() }
 
-    // TODO: Implement this; current implementation does not finish
-    override fun part2(): String {
-        val inputX = circuit.wiresX.toBitString { circuit.getSignal(it)!! }
-        val inputY = circuit.wiresY.toBitString { circuit.getSignal(it)!! }
-        val expectZ = operator(inputX, inputY)
-
-        return circuit.findErrors(operator)
-            .also { println("Found ${it.size} candidates: $it") }
-            .combinations(2)
-            .filter { (a, b) -> a != b }
-            .toList()
-            .also { println("Found ${it.size} pairs: $it") }
-            .combinations(swapCount)
-            .filter { swaps -> swaps.flatten().allDistinct() }
-            .distinct()
-            .first { swaps -> circuit.swap(swaps).evaluate() == expectZ }
-            .flatten()
-            .sorted()
-            .joinToString(",")
-    }
+    override fun part2() = circuit.findErrors().toSortedSet().joinToString(",")
 
 
-    private data class Circuit(val startSignals: Map<String, Boolean>, val gates: List<Gate>) {
-        val wires: Set<String> = gates.flatMapTo(mutableSetOf()) { it.wires }
-        val wiresX: SortedSet<String> by lazy { wires.filter { it.startsWith('x') }.toDescendingSortedSet() }
-        val wiresY: SortedSet<String> by lazy { wires.filter { it.startsWith('y') }.toDescendingSortedSet() }
-        val wiresZ: SortedSet<String> by lazy { wires.filter { it.startsWith('z') }.toDescendingSortedSet() }
-
-        private val signals: MutableMap<String, Boolean> = startSignals.toMutableMap()
-        private val gatesByWireOut: Map<String, Gate> = gates.associateBy { it.wireOut }
-        private val gatesToIndex: Map<String, Int> = gates.map { it.wireOut }.associateWithIndex()
+    private data class Circuit(
+        val signals: MutableMap<String, Boolean>,
+        val gates: Map<String, Triple<String, String, String>>
+    ) {
+        private val maxZ = gates.keys.filter { it[0] == 'z' }.maxBy { it.drop(1).toInt() }
 
 
-        init {
-            require(wiresX.size == wiresY.size)
-        }
+        fun evaluate() = gates.keys.asSequence().filter { it[0] == 'z' }.sortedDescending().map { evaluate(it) }
 
+        private fun evaluate(out: String): Boolean =
+            signals.getOrPut(out) {
+                val (l, r, op) = gates[out]!!
 
-        fun getGate(wireOut: String): Gate? = gatesByWireOut[wireOut]
-
-        fun getSignal(wire: String): Boolean? = signals[wire]
-
-        fun evaluate(): String = wiresZ.toBitString { evaluateWire(it) }
-
-        fun evaluateWire(wire: String): Boolean =
-            signals.getOrPut(wire) {
-                val gate = gatesByWireOut[wire]!!
-                evaluateWire(gate.wireIn1)
-                evaluateWire(gate.wireIn2)
-
-                gate.run(signals[gate.wireIn1]!!, signals[gate.wireIn2]!!)
-            }
-
-        fun findErrors(operator: (String, String) -> String): Set<String> {
-            val inputSize = wiresX.size
-            return listOf(
-                "0".repeat(inputSize),
-                "1".repeat(inputSize),
-                "01".repeat(inputSize / 2).padEnd(inputSize, '0'),
-                "10".repeat(inputSize / 2).padEnd(inputSize, '1'),
-            ).combinations(2).flatMap { findErrors(operator, it[0], it[1]) }.toSet()
-        }
-
-        private fun findErrors(operator: (String, String) -> String, x: String, y: String): Set<String> {
-            val actualZ = copy(startSignals = x.bitStringToSignalMap("x") + y.bitStringToSignalMap("y")).evaluate()
-            val expectZ = operator(x, y).padStart(wiresZ.size, '0')
-
-            return expand(wiresZ.withIndex().filter { (idx, _) -> expectZ[idx] != actualZ[idx] }.map { (_, it) -> it })
-        }
-
-        private fun expand(wiresOut: Iterable<String>): Set<String> {
-            val expandedWiresOut = wiresOut.toMutableSet()
-            var border: Collection<String> = expandedWiresOut
-            while (true) {
-                border = border
-                    .flatMap { getGate(it)!!.wiresIn }
-                    .filter { !it.startsWith('x') && !it.startsWith('y') }
-                    .ifEmpty { break }
-                expandedWiresOut += border
-            }
-            return expandedWiresOut
-        }
-
-        fun swap(swaps: List<List<String>>): Circuit {
-            val newGates = gates.toMutableList()
-            swaps.forEach { (from, to) ->
-                val fromIdx = gatesToIndex[from]!!
-                val toIdx = gatesToIndex[to]!!
-                val fromGate = newGates[fromIdx]
-                val toGate = newGates[toIdx]
-                newGates[fromIdx] = fromGate.copy(wireOut = toGate.wireOut)
-                newGates[toIdx] = toGate.copy(wireOut = fromGate.wireOut)
-            }
-            return copy(gates = newGates)
-        }
-    }
-
-
-    private interface Gate {
-        val type: String
-        val wireIn1: String
-        val wireIn2: String
-        val wireOut: String
-        val wiresIn: List<String> get() = listOf(wireIn1, wireIn2)
-        val wires: List<String> get() = listOf(wireIn1, wireIn2, wireOut)
-
-
-        fun copy(
-            wireIn1: String = this.wireIn1,
-            wireIn2: String = this.wireIn2,
-            wireOut: String = this.wireOut,
-        ): Gate = from(wireIn1, wireIn2, wireOut, this.type)
-
-        fun run(signalIn1: Boolean, signalIn2: Boolean): Boolean
-
-
-        companion object {
-            fun from(wireIn1: String, wireIn2: String, wireOut: String, type: String) =
-                when (type) {
-                    "AND" -> AndGate(Triple(wireIn1, wireIn2, wireOut))
-                    "OR" -> OrGate(Triple(wireIn1, wireIn2, wireOut))
-                    "XOR" -> XorGate(Triple(wireIn1, wireIn2, wireOut))
-                    else -> error("Unknown gate type '$type'.")
+                val lVal = evaluate(l)
+                val rVal = evaluate(r)
+                when (op) {
+                    "AND" -> lVal and rVal
+                    "OR" -> lVal or rVal
+                    "XOR" -> lVal xor rVal
+                    else -> error("Unknown gate operator '$op'.")
                 }
-        }
-    }
+            }
 
-    @JvmInline
-    private value class AndGate(private val descriptor: Triple<String, String, String>) : Gate {
-        override val type: String get() = "AND"
-        override val wireIn1: String get() = descriptor.first
-        override val wireIn2: String get() = descriptor.second
-        override val wireOut: String get() = descriptor.third
+        fun findErrors() =
+            gates
+                .filter { (out, params) ->
+                    val (l, r, op) = params
+                    val next = gates.filterValues { (l2, r2, _) -> out == l2 || out == r2 }
 
-        override fun run(signalIn1: Boolean, signalIn2: Boolean): Boolean = signalIn1 && signalIn2
-    }
-
-    @JvmInline
-    private value class OrGate(private val descriptor: Triple<String, String, String>) : Gate {
-        override val type: String get() = "OR"
-        override val wireIn1: String get() = descriptor.first
-        override val wireIn2: String get() = descriptor.second
-        override val wireOut: String get() = descriptor.third
-
-        override fun run(signalIn1: Boolean, signalIn2: Boolean): Boolean = signalIn1 || signalIn2
-    }
-
-    @JvmInline
-    private value class XorGate(private val descriptor: Triple<String, String, String>) : Gate {
-        override val type: String get() = "XOR"
-        override val wireIn1: String get() = descriptor.first
-        override val wireIn2: String get() = descriptor.second
-        override val wireOut: String get() = descriptor.third
-
-        override fun run(signalIn1: Boolean, signalIn2: Boolean): Boolean = signalIn1 != signalIn2
+                    (op != "XOR" && out[0] == 'z' && out !== maxZ) ||
+                        (op == "XOR" && out[0] != 'z' && l[0] !in "xy" && r[0] !in "xy") ||
+                        (op == "AND" && l != "x00" && r != "x00" && next.any { (_, it) -> it.third != "OR" }) ||
+                        (op == "XOR" && next.any { (_, it) -> it.third == "OR" })
+                }
+                .keys
     }
 }
-
-fun <T> Iterable<T>.toBitString(map: (T) -> Boolean): String = joinToString("") { if (map(it)) "1" else "0" }
-
-fun Long.toBitString(): String = toString(2)
-
-fun String.bitStringToLong(): Long = fold(0L) { acc, char -> acc * 2L + (char == '1').toLong() }
-
-fun String.bitStringToSignalMap(prefix: String): Map<String, Boolean> =
-    reversed().mapIndexed { idx, value -> "$prefix${idx.toString().padStart(2, '0')}" to (value == '1') }.toMap()
-
-fun <T : Comparable<T>> Iterable<T>.toDescendingSortedSet(): SortedSet<T> = toSortedSet { a, b -> -a.compareTo(b) }
 
 
 fun main() = Day24().run()
--- aoc/src/test/kotlin/com/fwdekker/aoc/y2024/Day24Test.kt
+++ aoc/src/test/kotlin/com/fwdekker/aoc/y2024/Day24Test.kt
@@ -6,17 +6,12 @@ import io.kotest.core.annotation.Tags
 
 @Tags("2024")
 object Day24Test : ChallengeTest(
-    { sample ->
-        if (sample == 3) Day24(sample, swapCount = 2, operator = { a, b ->
-            a.zip(b) { a1, b1 -> a1 == '1' && b1 == '1' }.toBitString { it }
-        })
-        else Day24(sample)
-    },
+    ::Day24,
     listOf(
         case(part = 1, sample = 1) to 4L,
         case(part = 1, sample = 2) to 2024L,
-        case(part = 2, sample = 3) to "z00,z01,z02,z05",
+//        case(part = 2, sample = 3) to "z00,z01,z02,z05", // Sample uses incompatible kind of circuit
         case(part = 1) to 53258032898766L,
-        case(part = 2) to "TODO",
+        case(part = 2) to "gbs,hwq,thm,wrm,wss,z08,z22,z29",
     )
 )
